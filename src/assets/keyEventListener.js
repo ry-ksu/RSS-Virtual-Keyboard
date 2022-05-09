a31dfca91fdbc141e5f849cb1d1b9fca89874987
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-05-09 16:26:05 +0300

    feat: add switching keyboard layouts

--- src/assets/keyEventListener.js
+++ src/assets/keyEventListener.js
@@ -3,7 +3,7 @@
 const keyEventListener = () => {
   let TEXTAREA = document.querySelector('.textarea');
 
-  // EventListener for Backspace
+  // EventListener for a Backspace
   const keyBackspace = () => {
     if (document.querySelector('textarea:focus')) {
       if (!TEXTAREA.value === '') {
@@ -15,9 +15,70 @@ const keyEventListener = () => {
     TEXTAREA.value = TEXTAREA.value.replace(/.$/, '');
   };
 
-  // EventListener for Tab
+  // EventListener for a Tab
   const keyTab = () => {
     TEXTAREA.value += '    ';
+  };
+  // EventListener for LeftAlt and LeftCtrl
+  const LeftAltCtr = () => {
+    let ALL_RU_CLASSES = document.querySelectorAll('.ru');
+    let ALL_EN_CLASSES = document.querySelectorAll('.en');
+    console.log(document.querySelectorAll('.en')[0]);
+    // If class 'en' is hidden
+    if (document.querySelector('.en').classList[1] === 'hidden') {
+      for (let i = 0; i < ALL_RU_CLASSES.length; i += 1) {
+        ALL_EN_CLASSES[i].classList.value = 'en';
+        ALL_RU_CLASSES[i].classList.value += ' hidden';
+      }
+      if (document.querySelector('.ru').querySelector('.caseDown').classList[1] !== 'hidden') {
+        for (let i = 0; i < ALL_EN_CLASSES.length; i += 1) {
+          ALL_EN_CLASSES[i].querySelector('.caseDown').classList.value = 'caseDown';
+          ALL_RU_CLASSES[i].querySelector('.caseDown').classList.value += ' hidden';
+        }
+      } else if (document.querySelector('.ru').querySelector('.caseUp').classList[1] !== 'hidden') {
+        for (let i = 0; i < ALL_EN_CLASSES.length; i += 1) {
+          ALL_EN_CLASSES[i].querySelector('.caseUp').classList.value = 'caseUp';
+          ALL_RU_CLASSES[i].querySelector('.caseUp').classList.value += ' hidden';
+        }
+      } else if (document.querySelector('.ru').querySelector('.caps').classList[1] !== 'hidden') {
+        for (let i = 0; i < ALL_EN_CLASSES.length; i += 1) {
+          ALL_EN_CLASSES[i].querySelector('.caps').classList.value = 'caps';
+          ALL_RU_CLASSES[i].querySelector('.caps').classList.value += ' hidden';
+        }
+      } else if (document.querySelector('.ru').querySelector('.shiftCaps').classList[1] !== 'hidden') {
+        for (let i = 0; i < ALL_EN_CLASSES.length; i += 1) {
+          ALL_EN_CLASSES[i].querySelector('.shiftCaps').classList.value = 'shiftCaps';
+          ALL_RU_CLASSES[i].querySelector('.shiftCaps').classList.value += ' hidden';
+        }
+      }
+    // If class 'ru' is hidden
+    } else {
+      for (let i = 0; i < ALL_EN_CLASSES.length; i += 1) {
+        ALL_RU_CLASSES[i].classList.value = 'ru';
+        ALL_EN_CLASSES[i].classList.value += ' hidden';
+      }
+      if (document.querySelector('.en').querySelector('.caseDown').classList[1] !== 'hidden') {
+        for (let i = 0; i < ALL_RU_CLASSES.length; i += 1) {
+          ALL_RU_CLASSES[i].querySelector('.caseDown').classList.value = 'caseDown';
+          ALL_EN_CLASSES[i].querySelector('.caseDown').classList.value += ' hidden';
+        }
+      } else if (document.querySelector('.en').querySelector('.caseUp').classList[1] !== 'hidden') {
+        for (let i = 0; i < ALL_RU_CLASSES.length; i += 1) {
+          ALL_RU_CLASSES[i].querySelector('.caseUp').classList.value = 'caseUp';
+          ALL_EN_CLASSES[i].querySelector('.caseUp').classList.value += ' hidden';
+        }
+      } else if (document.querySelector('.en').querySelector('.caps').classList[1] !== 'hidden') {
+        for (let i = 0; i < ALL_RU_CLASSES.length; i += 1) {
+          ALL_RU_CLASSES[i].querySelector('.caps').classList.value = 'caps';
+          ALL_EN_CLASSES[i].querySelector('.caps').classList.value += ' hidden';
+        }
+      } else if (document.querySelector('.en').querySelector('.shiftCaps').classList[1] !== 'hidden') {
+        for (let i = 0; i < ALL_RU_CLASSES.length; i += 1) {
+          ALL_RU_CLASSES[i].querySelector('.shiftCaps').classList.value = 'shiftCaps';
+          ALL_EN_CLASSES[i].querySelector('.shiftCaps').classList.value += ' hidden';
+        }
+      }
+    }
   };
 
   window.onkeydown = (e) => {
@@ -40,15 +101,16 @@ const keyEventListener = () => {
       || e.code === 'NumpadDecimal' || e.code === 'Numpad0' || e.code === 'Numpad1'
       || e.code === 'Numpad2' || e.code === 'Numpad3' || e.code === 'Numpad4'
       || e.code === 'Numpad6' || e.code === 'Numpad7' || e.code === 'Numpad8'
-      || e.code === 'Numpad9' || e.code === 'Numpad5'
+      || e.code === 'Numpad9' || e.code === 'Numpad5' || e.code === 'AltLeft'
     ) {
       return false;
     }
     return true;
   };
 
-  // Determine virtual key
+  // Determine virtual a key
   const determineKey = (key) => {
+    // Determine and disable a missing key
     if (
       key === 'Escape' || key === 'Pause' || key === 'Home' || key === 'PageUp'
       || key === 'PageDown' || key === 'End' || key === 'F1'
@@ -64,18 +126,40 @@ const keyEventListener = () => {
     ) {
       return false;
     }
+    // Animation of pressing a key
+    let KEY = document.querySelector('.' + key);
+    let KEY_RU = KEY.querySelector('.ru');
+    let KEY_EN = KEY.querySelector('.en');
+
+    KEY.classList.add('active');
+    document.addEventListener('keyup', () => {
+      KEY.classList.remove('active');
+    });
+    document.addEventListener('mouseup', () => {
+      KEY.classList.remove('active');
+    });
+
+    // Determine an available key
+    if (key === 'ControlLeft') {
+      if (document.querySelector('.AltLeft').classList[2] === 'active') {
+        LeftAltCtr();
+      }
+    } if (key === 'AltLeft') {
+      if (document.querySelector('.ControlLeft').classList[2] === 'active') {
+        LeftAltCtr();
+      }
+    }
     if (key === 'Backspace') {
       keyBackspace();
     } else if (key === 'Tab') {
       keyTab();
+    } else if (key === 'AltLeft' || key === 'ControlLeft') {
+      return false;
     } else {
-      let KEY = document.querySelector('.' + key);
-      let KEY_RU = document.querySelector('.' + key).querySelector('.ru');
       let KEY_RU_CASED = KEY_RU.querySelector('.caseDown');
       let KEY_RU_CASEU = KEY_RU.querySelector('.caseUp');
       let KEY_RU_CAPS = KEY_RU.querySelector('.caps');
       let KEY_RU_SH_CAPS = KEY_RU.querySelector('.shiftCaps');
-      let KEY_EN = document.querySelector('.' + key).querySelector('.en');
       let KEY_EN_CASED = KEY_EN.querySelector('.caseDown');
       let KEY_EN_CASEU = KEY_EN.querySelector('.caseUp');
       let KEY_EN_CAPS = KEY_EN.querySelector('.caps');
@@ -102,18 +186,11 @@ const keyEventListener = () => {
           TEXTAREA.value += KEY_EN_SH_CAPS.innerHTML;
         }
       }
-      KEY.classList.add('active');
-      document.addEventListener('keyup', () => {
-        KEY.classList.remove('active');
-      });
-      document.addEventListener('mouseup', () => {
-        KEY.classList.remove('active');
-      });
     }
     return true;
   };
 
-  // Add EventListener for physical keyboard
+  // Add EventListener for a physical keyboard
   document.addEventListener('keydown', (event) => {
     determineKey(event.code);
     if (document.querySelector('textarea:focus')) {
@@ -123,7 +200,7 @@ const keyEventListener = () => {
     }
   });
 
-  // Add EventListener for virtual keyboard
+  // Add EventListener for a virtual keyboard
   document.querySelector('.keyboard').addEventListener('mousedown', (event) => {
     let keyClass = event.target.closest('div').className;
     let key = keyClass.split(' ')[1];
