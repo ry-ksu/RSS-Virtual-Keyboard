41cf2e471d7d5ce86b8200d88fcf21a90005e43c
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-05-10 23:38:08 +0300

    fix: fix the algorithm 'Enter', 'Backspace', 'Del'

--- src/assets/keyEventListener.js
+++ src/assets/keyEventListener.js
@@ -12,31 +12,39 @@ const keyEventListener = () => {
 
   // EventListener for a Del
   const keyDel = () => {
-    let position = TEXTAREA.selectionStart;
-    TEXTAREA.value = TEXTAREA.value.slice(0, position) + TEXTAREA.value.slice(position + 1);
     TEXTAREA.focus();
+    if (TEXTAREA.selectionStart === TEXTAREA.selectionEnd) {
+      TEXTAREA.selectionEnd += 1;
+    }
+    TEXTAREA.setRangeText('');
   };
 
   // EventListener for a Backspace
   const keyBackspace = () => {
     TEXTAREA.focus();
-    let position = TEXTAREA.selectionStart;
-    TEXTAREA.value = TEXTAREA.value.slice(0, position - 1) + TEXTAREA.value.slice(position);
+    if (TEXTAREA.selectionStart === 0) {
+      //
+    } else if (TEXTAREA.selectionStart === TEXTAREA.selectionEnd) {
+      TEXTAREA.selectionStart -= 1;
+    }
+    TEXTAREA.setRangeText('');
   };
 
   // EventListener for a Enter
   const keyEnter = () => {
-    if (!(document.querySelector('textarea:focus'))) {
-      document.querySelector('.textarea').focus();
-    } else {
-      return false;
+    TEXTAREA.focus();
+    let positionStart = TEXTAREA.selectionStart;
+    TEXTAREA.setRangeText('\n');
+    if (positionStart === TEXTAREA.selectionStart) {
+      TEXTAREA.selectionStart += 1;
     }
-    return true;
   };
 
   // EventListener for a Tab
   const keyTab = () => {
-    TEXTAREA.value += '    ';
+    TEXTAREA.focus();
+    TEXTAREA.setRangeText('    ');
+    TEXTAREA.selectionStart += 4;
   };
 
   // Check language in Local Storage
@@ -50,30 +58,8 @@ const keyEventListener = () => {
     AltCtr();
   }
 
-  window.onkeydown = (e) => {
-    if (e.code === 'Tab') {
-      if (document.querySelector('textarea:focus')) {
-        keyTab();
-        TEXTAREA.value = TEXTAREA.value.substring(0, TEXTAREA.value.length - 4);
-        return false;
-      }
-    }
-    if (
-      e.code === 'Escape' || e.code === 'Pause' || e.code === 'Home' || e.code === 'PageUp'
-      || e.code === 'PageDown' || e.code === 'End' || e.code === 'F1'
-      || e.code === 'F2' || e.code === 'F3' || e.code === 'F4' || e.code === 'F5'
-      || e.code === 'F6' || e.code === 'F7' || e.code === 'F8' || e.code === 'F9'
-      || e.code === 'F10' || e.code === 'F11' || e.code === 'F12' || e.code === 'ContextMenu'
-      || e.code === 'NumLock' || e.code === 'NumpadDivide' || e.code === 'NumpadMultiply'
-      || e.code === 'NumpadSubtract' || e.code === 'NumpadAdd' || e.code === 'NumpadEnter'
-      || e.code === 'NumpadDecimal' || e.code === 'Numpad0' || e.code === 'Numpad1'
-      || e.code === 'Numpad2' || e.code === 'Numpad3' || e.code === 'Numpad4'
-      || e.code === 'Numpad6' || e.code === 'Numpad7' || e.code === 'Numpad8'
-      || e.code === 'Numpad9' || e.code === 'Numpad5' || e.code === 'AltLeft' || e.code === 'AltRight'
-    ) {
-      return false;
-    }
-    return true;
+  window.onkeydown = () => {
+    return false;
   };
 
   // Determine virtual a key
@@ -81,10 +67,10 @@ const keyEventListener = () => {
     // Determine and disable a missing key
     if (
       key === 'Escape' || key === 'Pause' || key === 'Home' || key === 'PageUp'
-      || key === 'PageDown' || key === 'End' || key === 'F1'
+      || key === 'PageDown' || key === 'End' || key === 'F1' || key === 'ScrollLock'
       || key === 'F2' || key === 'F3' || key === 'F4' || key === 'F5'
       || key === 'F6' || key === 'F7' || key === 'F8' || key === 'F9'
-      || key === 'F10' || key === 'F11' || key === 'F12'
+      || key === 'F10' || key === 'F11' || key === 'F12' || key === 'MetaLeft'
       || key === 'NumLock' || key === 'NumpadDivide' || key === 'NumpadMultiply'
       || key === 'NumpadSubtract' || key === 'NumpadAdd' || key === 'NumpadEnter'
       || key === 'NumpadDecimal' || key === 'Numpad0' || key === 'Numpad1'
@@ -124,22 +110,14 @@ const keyEventListener = () => {
       }
     } else if (key === 'Enter') {
       keyEnter();
-    } else if (key === 'ArrowUp') {
-      TEXTAREA.value += '▲';
-    } else if (key === 'ArrowRight') {
-      TEXTAREA.value += '►';
-    } else if (key === 'ArrowDown') {
-      TEXTAREA.value += '▼';
-    } else if (key === 'ArrowLeft') {
-      TEXTAREA.value += '◄';
     } else if (key === 'Delete') {
       keyDel();
     } else if (key === 'Backspace') {
       keyBackspace();
     } else if (key === 'Tab') {
       keyTab();
-    } else if (key === 'AltLeft' || key === 'ControlLeft' || key === 'ContextMenu'
-    || key === 'AltRight' || key === 'ControlRight' || key === 'MetaLeft') {
+    } else if (key === 'AltLeft' || key === 'ControlLeft'
+    || key === 'AltRight' || key === 'ControlRight') {
       return false;
     } else if ((key === 'ShiftRight' || key === 'ShiftLeft')
     && document.querySelector('.CapsLock').classList[2] === 'active') {
@@ -161,23 +139,39 @@ const keyEventListener = () => {
 
       if (KEY_RU.classList[1] === undefined) {
         if (KEY_RU_CASED.classList[1] === undefined) {
-          TEXTAREA.value += KEY_RU_CASED.innerHTML;
+          TEXTAREA.focus();
+          TEXTAREA.setRangeText(KEY_RU_CASED.innerHTML);
+          TEXTAREA.selectionStart += 1;
         } else if (KEY_RU_CASEU.classList[1] === undefined) {
-          TEXTAREA.value += KEY_RU_CASEU.innerHTML;
+          TEXTAREA.focus();
+          TEXTAREA.setRangeText(KEY_RU_CASEU.innerHTML);
+          TEXTAREA.selectionStart += 1;
         } else if (KEY_RU_CAPS.classList[1] === undefined) {
-          TEXTAREA.value += KEY_RU_CAPS.innerHTML;
+          TEXTAREA.focus();
+          TEXTAREA.setRangeText(KEY_RU_CAPS.innerHTML);
+          TEXTAREA.selectionStart += 1;
         } else {
-          TEXTAREA.value += KEY_RU_SH_CAPS.innerHTML;
+          TEXTAREA.focus();
+          TEXTAREA.setRangeText(KEY_RU_SH_CAPS.innerHTML);
+          TEXTAREA.selectionStart += 1;
         }
       } else if (KEY_EN.classList[1] === undefined) {
         if (KEY_EN_CASED.classList[1] === undefined) {
-          TEXTAREA.value += KEY_EN_CASED.innerHTML;
+          TEXTAREA.focus();
+          TEXTAREA.setRangeText(KEY_EN_CASED.innerHTML);
+          TEXTAREA.selectionStart += 1;
         } else if (KEY_EN_CASEU.classList[1] === undefined) {
-          TEXTAREA.value += KEY_EN_CASEU.innerHTML;
+          TEXTAREA.focus();
+          TEXTAREA.setRangeText(KEY_EN_CASEU.innerHTML);
+          TEXTAREA.selectionStart += 1;
         } else if (KEY_EN_CAPS.classList[1] === undefined) {
-          TEXTAREA.value += KEY_EN_CAPS.innerHTML;
+          TEXTAREA.focus();
+          TEXTAREA.setRangeText(KEY_EN_CAPS.innerHTML);
+          TEXTAREA.selectionStart += 1;
         } else {
-          TEXTAREA.value += KEY_EN_SH_CAPS.innerHTML;
+          TEXTAREA.focus();
+          TEXTAREA.setRangeText(KEY_EN_SH_CAPS.innerHTML);
+          TEXTAREA.selectionStart += 1;
         }
       }
     }
@@ -186,22 +180,7 @@ const keyEventListener = () => {
 
   // Add EventListener for a physical keyboard
   document.addEventListener('keydown', (event) => {
-    if (event.code === 'Backspace') {
-      TEXTAREA.value += TEXTAREA.value[TEXTAREA.value.length - 1];
-      let position = TEXTAREA.selectionStart;
-      TEXTAREA.value = TEXTAREA.value.slice(0, position - 1) + TEXTAREA.value.slice(position + 1);
-      TEXTAREA.focus();
-    } else {
-      determineKey(event.code);
-      if (document.querySelector('textarea:focus')) {
-        if (!(event.code === 'Tab') && !(event.code === 'Backspace') && !(event.code === 'Enter') && !(event.code === 'Delete')
-        && !(event.code === 'CapsLock') && !(event.code === 'ShiftLeft') && !(event.code === 'ControlLeft')
-        && !(event.code === 'MetaLeft') && !(event.code === 'AltLeft') && !(event.code === 'AltRight')
-        && !(event.code === 'ControlRight') && !(event.code === 'ShiftRight') && !(event.code === 'ContextMenu')) {
-          TEXTAREA.value = TEXTAREA.value.replace(/.$/, '');
-        }
-      }
-    }
+    determineKey(event.code);
   });
 
   // Add EventListener for a virtual keyboard
@@ -210,11 +189,7 @@ const keyEventListener = () => {
     let key = keyClass.split(' ')[1];
     if (keyClass.includes(key)) {
       determineKey(key);
-      if (document.querySelector('textarea:focus')) {
-        return false;
-      }
     }
-    return true;
   });
 };
 
